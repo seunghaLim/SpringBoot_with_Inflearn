d315cd524ecd1fd47ac61265796326dbefc1bf72
seungha <tmdgk490255@gmail.com>
2022-01-09 02:26:42 +0900

    2022.01.08 7일차 스프링부트 기본

---
+++ core/core/src/test/java/heiil/core/scope/SingletonWithPrototype.java
@@ -0,0 +1,76 @@
+package heiil.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class SingletonWithPrototype {
+
+
+    @Test
+    void singletonClientUsePrototype(){
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
+        
+        //client1과 client2는 같은 애가 반환됨 (포로토타입 빈이라고 하더라도 싱글톤 빈 안에 있어서)
+
+
+
+    }
+
+    static class ClientBean{
+        private final PrototypeBean prototypeBean; // 생성시점에 주입됨. 계속 같은 걸 씀
+
+        @Autowired
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+
+        public int logic(){
+
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
+
+    }
+
+    @Scope("prototype") // @Scope 어노테이션 붙이면 따로 @Component 안붙여도 자동으로 빈으로 등록됨
+    static class PrototypeBean{
+
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy" + this);
+        }
+    }
+}

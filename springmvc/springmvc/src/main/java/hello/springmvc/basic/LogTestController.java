7ec281ffced0892df15284200c33d79d82082aa7
seungha <tmdgk490255@gmail.com>
2022-01-23 02:31:06 +0900

    2022.01.22 15일차 스프링부트 MVC1 기본 기능

---
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class LogTestController {
+
+    // 롬복이 제공하는 어노테이션 @Slf4j이게 있으면 아래 코드 생략 가능
+    //private final Logger log = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/log-test")
+    public String logTest(){
+        String name = "Spring";
+
+        log.trace("trace log={}", name);
+        log.debug("debug log={}", name);
+        log.info(" info log={}", name);
+        log.warn(" warn log={}", name);
+        log.error("error log={}", name);
+
+        return "ok";
+    }
+}
---
+++ springmvc/springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -0,0 +1,21 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MappingController {
+
+    public Logger log = LoggerFactory.getLogger((getClass()));
+
+    @RequestMapping(value = "/hello-basic", method = RequestMethod.GET)
+    public String helloBasic(){
+        log.info("helloBasic");
+        return "ok";
+    }
+
+
+}

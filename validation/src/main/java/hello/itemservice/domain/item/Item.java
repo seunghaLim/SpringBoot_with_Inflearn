71c385c8a76e05734115f430f467af8a389b1714
seungha <tmdgk490255@gmail.com>
2022-01-29 02:00:35 +0900

    2022.01.28 21일차 스프링부트 MVC2 검증

--- validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -1,13 +1,29 @@
 package hello.itemservice.domain.item;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
+// @ScriptAssert(lang="javascript", script = "_this.price * _this.quantity >= 10000")
 public class Item {
 
+    //@NotNull(groups = UpdateCheck.class)
     private Long id;
+
+    //@NotBlank(message = "공백X", groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
+
+    //@NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    //@Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
+
+    //@NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+    //@Max(value = 9999, groups = {SaveCheck.class})
     private Integer quantity;
 
     public Item() {
